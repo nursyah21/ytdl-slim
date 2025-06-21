31f02f6cc412ad4ce56c4ff3c1c56ce2a0309eac
nursyah21 <nursyahjaya@gmail.com>
2025-06-21 08:43:28 +0800

    fix bug

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 import yt_dlp
 
-yt_dlp.YoutubeDL().extract_info("https://www.youtube.com/watch?v=DNvfNrLCzLo")
-# print(yt_dlp)
+res = yt_dlp.YoutubeDL().extract_info("https://www.youtube.com/watch?v=DNvfNrLCzLo", False)
+print(res)
